ed5d209c7d1fac4a14b34f4e0cc318b32fa4549a
Singh <sswarnde@a483e7af62cb.ant.amazon.com>
2020-06-21 20:43:01 -0400

    Enhanced audit for server spec

--- auditing/scripts/audit_to_csv.ps1
+++ auditing/scripts/audit_to_csv.ps1
@@ -1,24 +1,37 @@
-Param([Parameter(Mandatory=$true)][string]$outputpath
-, [Parameter(Mandatory=$true)][string]$dbname
-, [Parameter(Mandatory=$true)][string]$auditpath
-, [Parameter(Mandatory=$true)][string]$servername)
+Param(
+    [Parameter(Mandatory=$true)][string]$script:configfile
+)
+
+
+function read-config()
+{
+   $config = Get-Content $script:configfile | out-string | ConvertFrom-Json 
+   $script:sqlserver=$config.config.sqlserver| Out-String
+   $script:sqlscripts=$config.config.sqlscriptpath | Out-String
+   $script:auditdata=$config.config.auditdata| Out-String
+   $script:auditout=$config.config.auditout| Out-String
+   $script:dbname=$config.config.dbname | Out-String
+   
+    $script:sqlserver=$script:sqlserver.Replace("`r`n","")
+    $script:sqlscripts=$script:sqlscripts.Replace("`r`n","")
+    $script:auditdata=$script:auditdata.Replace("`r`n","")
+    $script:auditout=$script:auditout.Replace("`r`n","")
+    $script:dbname=$script:dbname.Replace("`r`n","")
+
+
+}
+
 
 function getdatatocsv()
 {
-    $servernamefile = $servername -replace '\\', ' '
-    $filename=$outputpath+"\"+"sqlaudit_"+$servernamefile+"_" + $(((get-date).ToUniversalTime()).ToString("yyyyMMddTHHmmssfff"))
+    $servernamefile = $script:sqlserver.Replace( '\\', '_')
+    $filename=$script:auditout+"\"+"sqlaudit_"+$servernamefile+"_" + $(((get-date).ToUniversalTime()).ToString("yyyyMMddTHHmmssfff"))
     $finalfilename = $filename + ".csv"
      
-    Invoke-Sqlcmd -ServerInstance $servername -Query "exec dbo.auditextract '$auditpath'" -Database $dbname | Export-csv -Path $finalfilename -Delimiter "`t" -NoTypeInformation
+    Invoke-Sqlcmd -ServerInstance $script:sqlserver -Query "exec dbo.auditextract '$script:auditdata'" -Database $script:dbname | Export-csv -Path $finalfilename -Delimiter "`t" -NoTypeInformation
     
     
-    #$header =$outputpath+ "\" + "headers.head"
-    
-    #bcp "exec $dbname.dbo.auditextract '$auditpath'"  queryout "$filename" -T -w -C ACP  -S $servername 
-    
-    #cmd /c copy $header+$filename $finalfilename
-    #remove-item $filename
 }
-
+read-config
 getdatatocsv
 # -C OEM 
