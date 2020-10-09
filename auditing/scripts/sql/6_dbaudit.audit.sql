d341ebf27654aad450051694664657a0afe270d5
Singh <sswarnde@amazon.com>
2020-10-09 12:06:53 -0400

    Fixes for #1

--- auditing/scripts/sql/6_dbaudit.audit.sql
+++ auditing/scripts/sql/6_dbaudit.audit.sql
@@ -2,8 +2,8 @@
 :setvar SERVERAUDIT "chcmasteraudit"
 :setvar AUDITEDDB "DBToAudit"
 */
-
-USE awsec2auditing
+/* 
+USE master
 GO
 IF NOT EXISTS(select * from sys.database_audit_specifications where name='$(DBAUDITNAME)')
 BEGIN
@@ -13,3 +13,23 @@ BEGIN
     WITH (STATE = ON)
 END
 GO
+ */
+
+use master;
+
+declare @script varchar(max)
+set @script =''
+select @script = @script + ''
+
+
+use master;
+set nocount on
+ 
+declare @batchscript varchar(max)
+set @batchscript = ''
+select @batchscript = @batchscript + 'use [' + name + '];' + char(10) +
+'create database audit specification [$(DBAUDITNAME)_' + name + ']' + char(10) + 'for server audit [$(SERVERAUDIT)]' + char(10) +
+'add (select, insert, update, delete, execute, receive, references on database::' + name + ' by public)' + char(10) + 'with (state = on);' + char(10) + char(10)
+from sys.databases where name not in ('master', 'model', 'msdb', 'tempdb')
+ 
+exec (@batchscript) 
